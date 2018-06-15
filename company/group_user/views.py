e27ec0816e6b534896fff3e91b795b3adbea0525
TamNguyen <huutam.ptnk@gmail.com>
2018-06-15 23:06:53 +0700

    added views.py

--- company/group_user/views.py
+++ company/group_user/views.py
@@ -1,2 +1,16 @@
 from .models import User, Group
+from rest_framework import viewsets
+from company.group_user.serializers import UserSerializer, GroupSerializer
+
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+	"""
+	API endpoint that allows user to be viewed or edited
+	"""
+	queryset = User.objects.all().order_by('-date_joined')
+	serializer_class = UserSerializer
+	
+class GroupViewSet(viewsets.ModelViewSet):
+	queryset = Group.objects.all()
+	serializer_class = GroupSerializer
